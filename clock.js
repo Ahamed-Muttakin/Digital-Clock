75411f6d7d4bf492079a5c70dd96629fd1f31b34
Muttakin Ahamed Rokey <71687021+Ahamed-Muttakin@users.noreply.github.com>
2020-09-22 14:55:49 +0600

    Digital clock

---
+++ clock.js
@@ -0,0 +1,21 @@
+function ShowTime(){
+    let date =new Date();
+    let h= date.getHours();
+    let m=date.getMinutes();
+    let s=date.getSeconds();
+    let session="AM";
+    if(h==0){
+        h=12; 
+    }
+    if(h>12){
+        h=h-12;
+        session="PM";
+    }
+    h= (h<10) ? "0" +h: h;
+    m= (m<10) ? "0" +m: m;
+    s= (s<10) ? "0" +s: s;
+    let time= h + ":"+ m + ":"+ s +" "+session;
+    document.getElementById("Myclock").textContent=time;
+    setTimeout(ShowTime, 1000);
+}
+ShowTime();
